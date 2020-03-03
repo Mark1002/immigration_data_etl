6c9449393397f77916af3138a15a8f295cf0d4fc
Mark Chang <ak0922747401@gmail.com>
2020-03-03 23:26:49 +0800

    init sql query and add data mapping

--- sql_queries.py
+++ sql_queries.py
@@ -3,7 +3,24 @@
 immigration_table_create = (
     """
     CREATE TABLE IF NOT EXISTS immigration (
-
+        cicid    VARCHAR,
+        i94mon   INT,
+        i94yr    INT,
+        i94cit   VARCHAR,
+        i94res   VARCHAR,
+        i94addr  VARCHAR,
+        i94mode  VARCHAR,
+        i94port  VARCHAR,
+        i94visa  VARCHAR,
+        arrdate  VARCHAR,
+        depdate  VARCHAR,
+        biryear  VARCHAR,
+        gender   VARCHAR,
+        visatype VARCHAR,
+        fltno    VARCHAR,
+        airline  VARCHAR,
+        matflag  VARCHAR,
+        admnum   VARCHAR,
     )
     """
 )
