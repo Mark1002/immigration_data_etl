a2569d7b853c935e6de4696c0bbb64b97770dfd5
Mark Chang <ak0922747401@gmail.com>
2020-03-04 20:47:59 +0800

    add visa.txt mapping and add sql queries

--- sql_queries.py
+++ sql_queries.py
@@ -24,3 +24,50 @@ immigration_table_create = (
     )
     """
 )
+# from i94addrl.txt
+state_table_create = (
+    """
+    CREATE TABLE IF NOT EXISTS state (
+        state_code VARCHAR,
+        state_name VARCHAR
+    )
+    """
+)
+# from i94cntyl.txt
+country_table_create = (
+    """
+    CREATE TABLE IF NOT EXISTS country (
+        country_code VARCHAR,
+        country_name VARCHAR
+    )
+    """
+)
+# from i94model.txt
+transport_type_table_create = (
+    """
+    CREATE TABLE IF NOT EXISTS transport_type (
+        transport_code INT,
+        transport_name VARCHAR
+    )
+    """
+)
+# from i94prtl.txt
+airport_table_create = (
+    """
+    CREATE TABLE IF NOT EXISTS airport (
+        airport_code VARCHAR,
+        airport_name VARCHAR,
+        state_code VARCHAR
+    )
+    """
+)
+
+# from i94visa.txt
+visa_type_table_create = (
+    """
+    CREATE TABLE IF NOT EXISTS visa_type (
+        visa_code INT,
+        visa_name VARCHAR
+    )
+    """
+)
