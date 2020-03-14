ec6a6e1b269a9bfb077466b7eeee315c4dc540c5
Mark Chang <ak0922747401@gmail.com>
2020-03-14 23:30:58 +0800

    fix transform airport and reset to pip

--- dags/transform/airport.py
+++ dags/transform/airport.py
@@ -1,31 +1,30 @@
 """Transform to airport parquet."""
-import s3fs
 from collections import OrderedDict
 from pyspark.sql import DataFrame, Row
 
 
 def preprocess_airport() -> list:
     """Preprocess airport table."""
-    fs = s3fs.S3FileSystem(anon=True)
     result = []
-    with fs.open('<s3-bucket>/mapping/i94prtl.txt', 'r') as file:
-        for line in file:
-            line = line.replace("'", "").strip()
-            airport_code, name = tuple(
-                map(lambda x: x.strip(), line.split('='))
-            )
-            temp = list(map(lambda x: x.strip(), name.split(',')))
-            if len(temp) > 1:
-                state_code = temp[-1]
-                airport_name = ",".join(temp[:-1])
-            else:
-                airport_name = temp[0]
-                state_code = None
-            result.append({
-                'airport_code': airport_code,
-                'airport_name': airport_name,
-                'state_code': state_code
-            })
+    raw_text = spark.read.text('s3a://<s3-bucket>/data/mapping/i94prtl.txt')
+    raw_list = [str(row.value) for row in raw_text.collect()]
+    for line in raw_list:
+        line = line.replace("'", "").strip()
+        airport_code, name = tuple(
+            map(lambda x: x.strip(), line.split('='))
+        )
+        temp = list(map(lambda x: x.strip(), name.split(',')))
+        if len(temp) > 1:
+            state_code = temp[-1]
+            airport_name = ",".join(temp[:-1])
+        else:
+            airport_name = temp[0]
+            state_code = None
+        result.append({
+            'airport_code': airport_code,
+            'airport_name': airport_name,
+            'state_code': state_code
+        })
     return result
 
 
