43d29da1852b216ce427600f62da7a4c20678974
Mark Chang <ak0922747401@gmail.com>
2020-03-14 17:53:52 +0800

    update upload s3 module

--- upload_s3.py
+++ upload_s3.py
@@ -15,19 +15,47 @@ class S3UploadModule:
     def __init__(self):
         """Init."""
         self.s3_client = boto3.client('s3')
+        self.s3_bucket = config.get('S3', 'BUCKET_NAME')
 
     def upload_immigration_data(self):
         """Upload immigration sas file."""
-        pass
+        dir_path = '../../data/18-83510-I94-Data-2016/'
+        for file in os.listdir(dir_path):
+            self.s3_client.upload_file(
+                f'{dir_path}{file}', self.s3_bucket,
+                f'data/18-83510-I94-Data-2016/{file}'
+            )
 
     def upload_cities_demographics_data(self):
         """Upload us-cities-demographics.csv."""
-        pass
+        self.s3_client.upload_file(
+            'us-cities-demographics.csv', self.s3_bucket,
+            'data/us-cities-demographics.csv'
+        )
 
     def upload_mapping_data(self):
         """Upload mapping data."""
-        pass
+        for file in os.listdir('mapping'):
+            self.s3_client.upload_file(
+                file, self.s3_bucket, f'data/mapping/{file}'
+            )
 
     def upload_airport_data(self):
         """Upload airport-codes_csv.csv."""
-        pass
+        self.s3_client.upload_file(
+            'airport-codes_csv.csv', self.s3_bucket,
+            'data/airport-codes_csv.csv'
+        )
+
+
+def main():
+    """Main execute."""
+    s3 = S3UploadModule()
+    s3.upload_airport_data()
+    s3.upload_cities_demographics_data()
+    s3.upload_immigration_data()
+    s3.upload_mapping_data()
+
+
+if __name__ == '__main__':
+    main()
