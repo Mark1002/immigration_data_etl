4260f6f265505fdbf8c4627fb656d2cc3a0d4779
Mark Chang <ak0922747401@gmail.com>
2020-03-18 13:52:45 +0800

    add schedule interval

--- dags/imm_dag.py
+++ dags/imm_dag.py
@@ -8,11 +8,12 @@ from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': True,
+    'depends_on_past': False,
     'start_date': datetime(2016, 1, 1),
     'end_date': datetime(2016, 12, 1),
     'retries': 1,
     'retry_delay': timedelta(minutes=2),
+    'schedule_interval': '@monthly',
     'provide_context': True
 }
 
