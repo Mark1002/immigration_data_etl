147f1c8cace990645f13d54e7abe73d9f1167c6e
Mark Chang <ak0922747401@gmail.com>
2020-03-18 13:27:25 +0800

    set depends on past true

--- dags/imm_dag.py
+++ dags/imm_dag.py
@@ -8,10 +8,10 @@ from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
+    'depends_on_past': True,
     'start_date': datetime(2016, 1, 1),
     'end_date': datetime(2016, 12, 1),
-    'retries': 0,
+    'retries': 1,
     'retry_delay': timedelta(minutes=2),
     'provide_context': True
 }
